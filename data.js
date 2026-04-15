c0eaac9b0af8b617b21518aac70c7048e771057f
[Anushka Dhulap] <[anushkadhulap@gmail.com]>
2026-04-15 16:16:07 +0530

    personal details change

--- data.js
+++ data.js
@@ -12,9 +12,9 @@ const portfolioData = {
   // ----------------------------------------------------------
   // 🧑 PERSONAL INFO
   // ----------------------------------------------------------
-  name: "Chinmay Ingle",
-  firstName: "Chinmay",   // Used in the hero greeting
-  lastName: "Ingle",      // Displayed large in the hero section
+  name: "Anushka Dhulap",
+  firstName: "Anushka",   // Used in the hero greeting
+  lastName: "Dhulap",      // Displayed large in the hero section
   tagline: "Engineering Student & Aspiring Developer",
   resumeLink: "#",        // Link to your resume (Google Drive, etc.)
 
